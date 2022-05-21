5be80d58e537bd21ea4cc72bed0e6de86407b92b
Noryson <abassyornor.edem@lesoustechnology.com>
2022-05-21 16:10:23 +0100

    Add Project files

--- js/script.js
+++ js/script.js
@@ -1,285 +1,131 @@
+let tier1 = {
+    "name": "Tier 1",
+    "amount": "10000",
+    "interest": "7",
+    "members": []
+};
+
+let tier2 = {
+    "name": "Tier 2",
+    "amount": "20000",
+    "interest": "12",
+    "members": []
+};
+
+let tier3 = {
+    "name": "Tier 3",
+    "amount": "30000",
+    "interest": "25",
+    "members": []
+};
+
 $(document).ready(function(){
-        function preloaderFadeOutInit(){
-        setTimeout(
-          function()
-          {
-            $('.preloader').fadeOut(400);
-            $('body').attr('id','');
 
-            $('.hero__contentBody').animate({bottom: '0px'}, 1000);
-            $('.hero__contentFooter').animate({bottom: '0px'}, 1400);
-            $('.hero__linkSocial').animate({bottom: '0px'}, 1300);
-          }, 2000);
-        }
-        // Window load function
-        jQuery(window).on('load', function () {
-            (function ($) {
-                preloaderFadeOutInit();
-            })(jQuery);
-        });
+     $("#form").submit((e) => {
+        e.preventDefault();
+        let name = $("#name")[0].value;
+        let tier = $("#tier")[0].value
 
-        $(".projects__box").mouseenter(function(element){
-            $(this).children(".projects__overlay").fadeIn();
-            $(this).children(".projects__boxItem").addClass("projects__boxImageTransform");
+        let userName = $("#userName")[0].innerHTML;
+        let userTier = $("#userTier")[0];
+        let userAmount = $("#userAmount")[0];
+        let userInterest = $("#userInterest")[0];
+        let userReturns = $("#userReturns")[0];
 
-            $(this).find(".projects__linkHeader").removeClass("projects__linkHeaderTransformOut");
-            $(this).find(".projects__linkHeader").addClass("projects__linkHeaderTransformIn");
+        switch(tier){
+            case "1":
+                tier1.members.push(name);
+                $("#userName")[0].innerHTML = name;
+                $("#userTier")[0].innerHTML = tier1.name;
+                $("#userAmount")[0].innerHTML = tier1.amount;
+                $("#userInterest")[0].innerHTML = tier1.amount;
+                $("#userReturns")[0].innerHTML = parseInt(tier1.interest) /100 * parseInt(tier1.amount);
 
-            $(this).find(".projects__linkBody").removeClass("projects__linkBodyTransformOut");
-            $(this).find(".projects__linkBody").addClass("projects__linkBodyTransformIn");
-        });
-
-        $(".projects__box").mouseleave(function(element){
-            $(this).children(".projects__overlay").fadeOut();
-            $(this).children(".projects__boxItem").removeClass("projects__boxImageTransform");
-
-            $(this).find(".projects__linkHeader").addClass("projects__linkHeaderTransformOut");
-            $(this).find(".projects__linkHeader").removeClass("projects__linkHeaderTransformIn");
-
-            $(this).find(".projects__linkBody").addClass("projects__linkBodyTransformOut");
-            $(this).find(".projects__linkBody").removeClass("projects__linkBodyTransformIn");
-        });
-
-
-
-        /* ====================================================================
-         * Project Carousel
-         *
-         *--------------------------------------------------------------------- */
-
-         var images = new Array();
-         var currentIndex = 0;
-         var scale = 1.0;
-
-         $(".projectCarousel__open").click(function(){
-            startIndex = parseInt($(this)[0].getAttribute("datasrc"));
-            openCarousel(startIndex);
-            $(".projectCarousel").toggle();
-         });
-
-         $(".projectCarousel__close").click(function(){
-            closeCarousel();
-            $(".projectCarousel").toggle();
-         });
-
-         $(".projectCarousel__rightArrow").click(function(){
-            nextImage();
-         });
-
-         $(".projectCarousel__leftArrow").click(function(){
-            previousImage();
-         });
-
-         $(".projectCarousel__footerMenuBoxToggle").click(function(){
-            $(".projectCarousel__footer").toggleClass("projectCarousel__footerOpened");
-         });
-
-         $(".projectCarousel__toolbarZoomIn").click(function(){
-                scale += 0.1
-                $(".projectCarousel__image").css("transform", `scale(${scale})`);
-         });
-
-         $(".projectCarousel__toolbarZoomOut").click(function(){
-                if(scale > 1)
-                    scale -= 0.1
-                $(".projectCarousel__image").css("transform", `scale(${scale})`);
-         });
-
-         $(".projectCarousel__toolbarFullscreen").click(function(){
-                if (document.fullscreenElement) {
-                    document.exitFullscreen();
-                } else {
-                    document.documentElement.requestFullscreen();
+                $("#otherMembers").empty();
+                var total = 0;
+                for(let member of tier1.members){
+                    $("#otherMembers").append(`
+                      <tr>
+                        <td >${member}</td>
+                        <td >${tier1.name}</td>
+                        <td >${tier1.amount}</td>
+                        <td>${tier1.interest}</td>
+                        <td >${$("#userReturns")[0].innerHTML}</td>
+                      </tr>
+                    `);
+                    total += 10000;
                 }
-         });
 
-         $(".projectCarousel__toolbarPlay").click(function(){
-                play();
-                $(this).hide();
-                $(".projectCarousel__toolbarPause").show();
-         });
+                    $("#otherMembers").append(`
+                      <tr>
+                        <td>TOTAL</td>
+                        <td >${total}</td>
+                      </tr>
+                    `)
+                break;
+            case "2":
+                tier2.members.push(name);
+                $("#userName")[0].innerHTML = name;
+                $("#userTier")[0].innerHTML = tier2.name;
+                $("#userAmount")[0].innerHTML = tier2.amount;
+                $("#userInterest")[0].innerHTML = tier2.amount;
+                $("#userReturns")[0].innerHTML = parseInt(tier2.interest) /100 * parseInt(tier2.amount);
 
-         $(".projectCarousel__toolbarPause").click(function(){
-                pause();
-                $(this).hide();
-                $(".projectCarousel__toolbarPlay").show();
-         });
+                $("#otherMembers").empty();
+                var total = 0;
+                for(let member of tier2.members){
+                    $("#otherMembers").append(`
+                      <tr>
+                        <td >${member}</td>
+                        <td >${tier2.name}</td>
+                        <td >${tier2.amount}</td>
+                        <td>${tier2.interest}</td>
+                        <td >${$("#userReturns")[0].innerHTML}</td>
+                      </tr>
+                    `);
+                    total += 10000;
+                }
 
-         $(".projectCarousel__toolbarDownload").click(function(){
-              var filename = images[currentIndex];
-              var text = images[currentIndex];
-              var element = document.createElement('a');
-              element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-              element.setAttribute('download', filename);
-              element.style.display = 'none';
-              document.body.appendChild(element);
+                    $("#otherMembers").append(`
+                      <tr>
+                        <td>TOTAL</td>
+                        <td >${total}</td>
+                      </tr>
+                    `)
+                break;
+            case "3":
+                tier3.members.push(name);
+                $("#userName")[0].innerHTML = name;
+                $("#userTier")[0].innerHTML = tier3.name;
+                $("#userAmount")[0].innerHTML = tier3.amount;
+                $("#userInterest")[0].innerHTML = tier3.amount;
+                $("#userReturns")[0].innerHTML = parseInt(tier3.interest) /100 * parseInt(tier3.amount);
 
-              element.click();
-              document.body.removeChild(element);
-         });
+                $("#otherMembers").empty();
+                var total = 0;
+                for(let member of tier3.members){
+                    $("#otherMembers").append(`
+                      <tr>
+                        <td >${member}</td>
+                        <td >${tier3.name}</td>
+                        <td >${tier3.amount}</td>
+                        <td>${tier3.interest}</td>
+                        <td >${$("#userReturns")[0].innerHTML}</td>
+                      </tr>
+                    `);
+                    total += 10000;
+                }
 
-         $(".projectCarousel").mousemove(function(event){
-            event.stopImmediatePropagation();
-            $(".projectCarousel__header").fadeIn();
-            $(".projectCarousel__leftArrow").fadeIn();
-            $(".projectCarousel__rightArrow").fadeIn();
-            $(".projectCarousel__footer").fadeIn();
-
-            setTimeout(function(){
-                $(".projectCarousel__header").fadeOut();
-                $(".projectCarousel__leftArrow").fadeOut();
-                $(".projectCarousel__rightArrow").fadeOut();
-                $(".projectCarousel__footer").fadeOut();
-            }, 5000);
-         });
-
-         function openCarousel(index){
-            var newBox = ($(".projectCarousel__footerMenuBox")[0]).cloneNode(true);
-            $(".projectCarousel__footerMenuBox").remove();
-
-            for(i=0; i<$(".projects__boxItem").length; i++){
-                images[i] = $(".projects__boxItem")[i].src;
-                $(newBox).find(".projectCarousel__footerMenuBoxImage")[0].src = images[i];
-                $(newBox)[0].datasrc = i;
-                $(newBox).show();
-                $(".projectCarousel__footerMenu").append(newBox);
-                newBox = ($(".projectCarousel__footerMenuBox")[0]).cloneNode(true);
-            }
-            selectImage(index);
-
-             $(".projectCarousel__footerMenuBox").click(function(){
-                var newIndex = $(this)[0].datasrc;
-                selectImage(newIndex, newIndex > currentIndex? "right": "left");
-             });
-
-             $(".projectCarousel").mouseenter();
-         }
-
-         function closeCarousel(){
-            $(".projectCarousel__imageBox").hide();
-            $(".projectCarousel__toolbarPause").click();
-         }
-
-         function previousImage(){
-            if(currentIndex - 1 == -1){
-                currentIndex = images.length - 1;
-            }
-            else{
-                currentIndex--;
-            }
-            selectImage(currentIndex, "left");
-         }
-
-         function nextImage(){
-            if(currentIndex + 1 == images.length){
-                currentIndex = 0;
-            }
-            else{
-                currentIndex++;
-            }
-            selectImage(currentIndex, "right");
-         }
-
-         function selectImage(index, direction){
-            if(index >= images.length || index < 0)
+                    $("#otherMembers").append(`
+                      <tr>
+                        <td>TOTAL</td>
+                        <td >${total}</td>
+                      </tr>
+                    `)
+                break;
+            default:
+                alert("Wrong input");
                 return;
-
-            if(direction == "right"){
-                var dirValue1 = "-100%";
-                var dirValue2 = "100%";
-            }
-            else{
-                var dirValue1 = "100%";
-                var dirValue2 = "-100%";
-            }
-
-            if($(".projectCarousel__imageBox").length > 1)
-                $(".projectCarousel__imageBox")[0].remove();
-
-            var current = $(".projectCarousel__imageBox")[0];
-            var next = current.cloneNode(true);
-
-            $(next).find(".projectCarousel__image")[0].src = images[index];
-            $(next).removeAttr('style');
-            $(next).css({right: dirValue1, left: dirValue2});
-            $(next).show();
-            $(".projectCarousel").append(next);
-
-            $(current).animate({right: dirValue2, left: dirValue1});
-            $(next).animate({right: "0%", left: "0%"});
-
-            currentIndex = index;
-            updateLabels();
-            $(".projectCarousel__footerMenuBox").removeClass("projectCarousel__footerMenuBoxSelected");
-            $($(".projectCarousel__footerMenuBox")[index]).addClass("projectCarousel__footerMenuBoxSelected");
-         }
-
-         function updateLabels(){
-            $(".projectCarousel__projectNumber")[0].innerHTML = `${currentIndex + 1} / ${images.length}`
-
-            $(".projectCarousel__footerDetailsHeader")[0].innerHTML =
-                        $(".projects__linkHeaderText")[currentIndex * 2 + 1].innerHTML;
-
-            $(".projectCarousel__footerDetailsBody")[0].innerHTML =
-                        $(".projects__description")[currentIndex].innerHTML;
-
-            $(".projectCarousel__footerDetailsLink")[0].href =
-                        $(".projects__description")[currentIndex].href;
-         }
-
-         var timer, width;
-         function play(){
-            moveProgressBar();
-            timer = setInterval(nextAndMove, 6000);
-         }
-
-         function pause(){
-            clearInterval(timer);
-            $("#progress").hide();
-            width = 100;
-         }
-
-         function nextAndMove(){
-            nextImage();
-            moveProgressBar();
-         }
-
-         async function moveProgressBar() {
-            var elem = document.getElementById("progressBar");
-            elem.style.width = "0%";
-            $("#progress").show();
-
-            width = 0;
-            for(; width<=100; width++){
-                elem.style.width = width + "%";
-                await sleep(50);
-            }
         }
-
-        async function sleep(ms){
-            return new Promise(resolve => setTimeout(resolve, ms));
-        }
-
-
-        /* ====================================================================
-         * Stats
-         *
-         *--------------------------------------------------------------------- */
-
-
-//        $(".stats__boxNumber").click(function(){
-        var boxes = $(".stats__boxNumber");
-        var noOfBoxes = boxes.length;
-            for(let i=0; i<noOfBoxes; i++){
-                count(boxes[i], boxes[i].innerHTML);
-            }
-//        });
-
-        async function count(box, number){
-            var delay = Math.floor(2000/(number/20));
-            for(let i=0; i<=number; i=i+20){
-                box.innerHTML = i;
-                await sleep(delay);
-            }
-        }
+     });
 });
